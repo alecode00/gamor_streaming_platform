f27fb9dad261efa3a7fe8e2d9b7d0c5a80bd09a8
alecode00 <alcoll000419@gmail.com>
2024-05-06 18:51:33 -0400

    Añadido los componentes de trendingCat y el LogIn

--- src/components/Home/home_trending_categories_components/TrendingCategories.jsx
+++ src/components/Home/home_trending_categories_components/TrendingCategories.jsx
@@ -1,96 +1,87 @@
 import { PropTypes } from "prop-types";
+import "./trendingCatg.css"
 
-export const TrendingCategories = ({handleChangeCategory}) => {
+export const TrendingCategories = ({ handleChangeCategory }) => {
 
   TrendingCategories.propTypes = {
     handleChangeCategory: PropTypes.func,
   }
 
-  const handleAsignAction = () =>{
+  const handleAsignAction = () => {
     handleChangeCategory("Action");
   }
-  const handleAsignSports = () =>{
+  const handleAsignSports = () => {
     handleChangeCategory("Sports");
   }
-  const handleAsignAdventure = () =>{
+  const handleAsignAdventure = () => {
     handleChangeCategory("Adventure");
   }
-  const handleAsignArcade = () =>{
+  const handleAsignArcade = () => {
     handleChangeCategory("Arcade");
   }
-  const handleAsignFantasy = () =>{
+  const handleAsignFantasy = () => {
     handleChangeCategory("Fantasy");
   }
-  const handleAsignStrategy = () =>{
+  const handleAsignStrategy = () => {
     handleChangeCategory("Strategy");
   }
-  const handleAsignShooter = () =>{
+  const handleAsignShooter = () => {
     handleChangeCategory("Shooter");
   }
-  const handleAsignAll = () =>{
+  const handleAsignAll = () => {
     handleChangeCategory("All");
   }
-  
+
 
   return (
     <>
-        <h3>Trending Categories</h3>
+      <h3>Trending Categories</h3>
 
-        <button onClick={handleAsignAction} >
-          <div>
-            <p>/1</p>
-            <h4>Action Games</h4>
-          </div>
-        </button>
+      <div className="buttonsDiv">
+        <div className="firstRowButtons">
+          <button onClick={handleAsignAction} className="button-trend">
+              <p>/1</p>
+              <h4>Action Games</h4>
+          </button>
 
-        <button onClick={handleAsignSports} >
-          <div>
-            <p>/2</p>
-            <h4>Sports Games</h4>
-          </div>
-        </button>
+          <button onClick={handleAsignSports} className="button-trend">
+              <p>/2</p>
+              <h4>Sports Games</h4>
+          </button>
 
-        <button onClick={handleAsignAdventure} >
-          <div>
-            <p>/3</p>
-            <h4>Adventure Games</h4>
-          </div>
-        </button>
+          <button onClick={handleAsignAdventure} className="button-trend">
+              <p>/3</p>
+              <h4>Adventure Games</h4>
+          </button>
 
-        <button onClick={handleAsignArcade} >
-          <div>
-            <p>/4</p>
-            <h4>Arcade Games</h4>
-          </div>
-        </button>
+          <button onClick={handleAsignArcade} className="button-trend">
+              <p>/4</p>
+              <h4>Arcade Games</h4>
+          </button>
+        </div>
 
-        <button onClick={handleAsignFantasy} >
-          <div>
+        <div className="secondRowButtons">
+        <button onClick={handleAsignFantasy} className="button-trend">
             <p>/5</p>
             <h4>Fantasy Games</h4>
-          </div>
         </button>
 
-        <button onClick={handleAsignStrategy} >
-          <div>
+        <button onClick={handleAsignStrategy} className="button-trend">
             <p>/6</p>
             <h4>Strategy Games</h4>
-          </div>
         </button>
 
-        <button onClick={handleAsignShooter} >
-          <div>
+        <button onClick={handleAsignShooter} className="button-trend">
             <p>/7</p>
             <h4>Shooter Games</h4>
-          </div>
         </button>
 
-        <button onClick={handleAsignAll} >
-          <div>
+        <button onClick={handleAsignAll} className="button-trend" >
             <p>View All</p>
             <h4>All Categories</h4>
-          </div>
         </button>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/logIn_components/LogIn.jsx
+++ src/components/logIn_components/LogIn.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-/* import "./LogIn.css"; */
+import "./LogIn.css";
 
 const usersBox = [
   {
@@ -35,7 +35,7 @@ export const LogIn = () => {
   let component = <h1></h1>;
   if (isLogin) {
     component = <h1>You have logged</h1>;
-  }else if(isTry){
+  } else if (isTry) {
     component = <h1>Your user is not registered</h1>
   }
 
@@ -44,9 +44,9 @@ export const LogIn = () => {
     setIsTry(true);
     usersBox.forEach((user) => {
       if (
-        event.target[0].value === user.username&&
+        event.target[0].value === user.username &&
         event.target[1].value === user.email &&
-        event.target[2].value === user.password 
+        event.target[2].value === user.password
       ) {
         setIsLogin(true);
       }
@@ -55,10 +55,7 @@ export const LogIn = () => {
 
   return (
     <>
-      <div className="container">
-        {/* <div className='space space-1' ></div>
-        <div className='space space-2' ></div>
-        <div className='space space-3' ></div> */}
+      {/* <div className="container">
         <form onSubmit={handleSubmit}>
           <input
             placeholder="Enter your username"
@@ -80,10 +77,43 @@ export const LogIn = () => {
           />
           <button type="submit">Log In</button>
         </form>
+      </div> */}
+
+      <div className="grid align__item">
+
+        <div className="register">
+
+          {/* <svg xmlns="http://www.w3.org/2000/svg" className="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412"><defs><linearGradient id="a" x1="0%" y1="0%" y2="0%"><stop offset="0%" stop-color="#8ceabb" /><stop offset="100%" stop-color="#378f7b" /></linearGradient></defs><path fill="url(#a)" d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z" /></svg> */}
+
+          <h1>Welcome Back</h1>
+
+          <form action="" method="post" className="form" onSubmit={handleSubmit}>
+
+            <div className="form__field">
+              <input type="username" placeholder="username" />
+            </div>
+
+            <div className="form__field">
+              <input type="email" placeholder="info@mailaddress.com" />
+            </div>
+
+            <div className="form__field">
+              <input type="password" placeholder="••••••••••••" />
+            </div>
+
+            <div className="form__field">
+              <input type="submit" value="Sign In" />
+            </div>
+
+          </form>
+          {component}
+        </div>
+
       </div>
-      {component}
 
       {/* {generateLayer()} */}
     </>
   );
 };
+
+{/*  */ }
